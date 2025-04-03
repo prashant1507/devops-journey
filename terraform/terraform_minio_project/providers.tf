49af551d73fb4dee9a7482f3bcb61cfe8dbf285c
Prashant Jeet Singh <pjeet.0733@gmail.com>
2025-04-03 10:05:52 +0400

    providers.tf

--- terraform/terraform_minio_project/providers.tf
+++ terraform/terraform_minio_project/providers.tf
@@ -8,7 +8,7 @@ provider "aws" {
     s3 = var.minio_url  # Example: "http://localhost:9000"
   }
 
-  s3_force_path_style = true
+  s3_use_path_style = true # (Optional) Whether to enable the request to use path-style addressing, i.e., https://s3.amazonaws.com/BUCKET/KEY. By default, the S3 client will use virtual hosted bucket addressing, https://BUCKET.s3.amazonaws.com/KEY, when possible. Specific to the Amazon S3 service.
   skip_credentials_validation = true # Skips AWS-specific credential checks.
   skip_metadata_api_check     = true # Disables AWS metadata API checks.
   skip_requesting_account_id  = true # Prevents account ID requests.
